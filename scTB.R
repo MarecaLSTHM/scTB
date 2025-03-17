02929e0bf8e2416d3b172efdc3c599e943112041
MarecaLSTHM <marecasitholeuk2@gmail.com>
2025-03-17 11:50:54 +0200

    Read in the data

---
+++ scTB.R
@@ -0,0 +1,5 @@
+# Read in the data from the births file and save it into births variable
+births <- read.table("births.csv", header = TRUE, sep=",")
+
+# Display the first few rows of the data
+head(births)
